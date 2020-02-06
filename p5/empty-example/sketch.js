af15dc72017e7562c3d3df3d63c789fed97a7633
Jeffrey Zhang <zjeffreyl@gmail.com>
2020-02-06 14:19:17 -0800

    Start Creating UI

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -8,7 +8,7 @@ function Block(x, y, side) {
   
 Block.prototype.show = function()
 {
-    var lightBlue = color(173,216,230);
+    let lightBlue = color(173,216,230);
     fill(this.brightness, 125);
     stroke(lightBlue);
     rect(this.x, this.y, this.side, this.side);
@@ -42,28 +42,65 @@ Block.prototype.rollover = function(mX, mY)
   }
 }
 
+function SelectorButton(x, y, color, text) {
+    this.x = x;
+    this.y = y;
+    this.color = color;
+    this.text = text;
+}
+  
+SelectorButton.prototype.show = function()
+{
+    this.createButton(this.text).style('color', this.color).style('border', '2px solid ' + this.color);
+    this.position(this.x, this.y);
+    this.mouseOver(this.onTop).mouseOut(this.outside);
+}
+
+
+SelectorButton.prototype.onTop = function() 
+{
+    this.style('color', '#fff').style('outline','0').style('transition', 'box-shadow 300ms ease-in-out, color 300ms ease-in-out').style('box-shadow', '0 0 40px 40px ' + this.color + ' inset');
+}
+
+SelectorButton.prototype.outside = function() 
+{
+    button.style('color', this.color).style('outline','0').style('transition', 'box-shadow 300ms ease-in-out, color 300ms ease-in-out').style('box-shadow', '0 0 40px 40px #fff inset'); 
+}
+
+
 let blocks = [];
+let buttonStart;
+let buttonFinish;
+let buttonObstacle;
 
 function setup() {
   createCanvas(600, 600);
   noStroke();
   rectMode(CENTER);
+  createButtons();
   //Create grid
   background(255);
-  for (var i = 20; i < height; i += 20) {
+  for (var i = 20; i < width; i += 20) {
     // draw one line of 20 rectangles across the x-axis
-    for (var j = 20; j < width; j += 20) {
-      let block = new Block(i,j,20);
+    for (var j = 40; j < height - 80; j += 20) {
+      let block = new Block(i,j + 80,20);
       blocks.push(block);
     }
   }
 }
 
 function mousePressed() {
-  for(let i = 0 ;i < blocks.length; i++)
+  for(let i = 0; i < blocks.length; i++)
   {
     blocks[i].clicked(mouseX, mouseY);
   }
+}
+
+function createButtons()
+{
+  buttonStart = SelectorButton(40, 30, '#1abc9c', 'Set Start');
+  buttonFinish = SelectorButton(140, 30, '#e74c3c', 'Set Finish');
+  buttonObstacle = SelectorButton(240, 30, '#2c3e50', 'Set Obstacle');
 }
 
 function draw() {
@@ -72,4 +109,9 @@ function draw() {
     blocks[i].rollover(mouseX, mouseY);
     blocks[i].show();
   }
+  // buttonStart.show();
+  // buttonFinish.show();
+  // buttonObstacle.show();
 }
+
+
